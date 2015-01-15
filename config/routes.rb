902919aed2c484fc383a99d290c51b931e14ed72
Michael Wagner <wagnerdmike@gmail.com>
2015-01-15 14:11:24 -0800

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root             'static_pages#home'
+  root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
@@ -10,4 +8,5 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
